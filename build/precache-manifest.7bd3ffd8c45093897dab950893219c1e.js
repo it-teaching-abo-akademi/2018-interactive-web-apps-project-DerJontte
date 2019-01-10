57e6d93a9437f0b2d7a86629c1ce2176442a7249
DerJontte <john.nordqvist@gmail-com>
2019-01-11 00:55:11 +0200

    Removed directory .idea and added build

---
+++ build/precache-manifest.7bd3ffd8c45093897dab950893219c1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7601404b6c6b6764a98bbe188b8eabb",
+    "url": "/it-teaching-abo-akademi/2018-interactive-web-apps-project-DerJontte/build/static/media/BusyGear.a7601404.svg"
+  },
+  {
+    "revision": "47f7f1c26182286da158",
+    "url": "/it-teaching-abo-akademi/2018-interactive-web-apps-project-DerJontte/build/static/js/runtime~main.47f7f1c2.js"
+  },
+  {
+    "revision": "1c0c622a1eab623f22a0",
+    "url": "/it-teaching-abo-akademi/2018-interactive-web-apps-project-DerJontte/build/static/js/main.1c0c622a.chunk.js"
+  },
+  {
+    "revision": "b5ca635f8f449c28ebfb",
+    "url": "/it-teaching-abo-akademi/2018-interactive-web-apps-project-DerJontte/build/static/js/1.b5ca635f.chunk.js"
+  },
+  {
+    "revision": "1c0c622a1eab623f22a0",
+    "url": "/it-teaching-abo-akademi/2018-interactive-web-apps-project-DerJontte/build/static/css/main.c76a582b.chunk.css"
+  },
+  {
+    "revision": "d8872a821949d68338a785552c04756d",
+    "url": "/it-teaching-abo-akademi/2018-interactive-web-apps-project-DerJontte/build/index.html"
+  }
+];

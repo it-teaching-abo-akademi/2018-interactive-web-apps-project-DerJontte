b55759f92e51c5f9a7582d930ca38198308cc068
DerJontte <john.nordqvist@gmail-com>
2019-01-11 01:07:59 +0200

    Updates

---
+++ precache-manifest.50357672e54806809296018982ebff5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7601404b6c6b6764a98bbe188b8eabb",
+    "url": "/2018-interactive-web-apps-project-DerJontte/static/media/BusyGear.a7601404.svg"
+  },
+  {
+    "revision": "d71a834e2b01b4798d3d",
+    "url": "/2018-interactive-web-apps-project-DerJontte/static/js/runtime~main.d71a834e.js"
+  },
+  {
+    "revision": "1c0c622a1eab623f22a0",
+    "url": "/2018-interactive-web-apps-project-DerJontte/static/js/main.1c0c622a.chunk.js"
+  },
+  {
+    "revision": "162f4a2eec89f1f37fcb",
+    "url": "/2018-interactive-web-apps-project-DerJontte/static/js/1.162f4a2e.chunk.js"
+  },
+  {
+    "revision": "1c0c622a1eab623f22a0",
+    "url": "/2018-interactive-web-apps-project-DerJontte/static/css/main.c76a582b.chunk.css"
+  },
+  {
+    "revision": "6cbd8f9bf29b2c640e07bf783b7f7884",
+    "url": "/2018-interactive-web-apps-project-DerJontte/index.html"
+  }
+];
